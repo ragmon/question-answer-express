76e15a7315ea8f0553e48841d7c49650a6567d77
Arthur Ragimov <arthur.ragimov@gmail.com>
2019-08-04 13:06:56 +0300

    Inital project version

---
+++ routes/api.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/question', function(req, res, next) {
+  //
+});
+
+router.get('/question/:id', function(req, res, next) {
+  //
+});
+
+router.post('/question', function(req, res, next) {
+  //
+});
+
+router.put('/question/:id', function(req, res, next) {
+  //
+});
+
+router.delete('/question/:id', function(req, res, next) {
+  //
+});
+
+router.post('/question/:id/rate_up', function(req, res, next) {
+  //
+});
+
+router.post('/question/:id/rate_down', function(req, res, next) {
+  //
+});
+
+router.post('/question/:id/answer', function(req, res, next) {
+  //
+});
+
+module.exports = router;
